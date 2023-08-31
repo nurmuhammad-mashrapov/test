9949e13f748498477b81a2ce25f5a44afdea1a0f
Nurmuhammad Mashrapov <nurmuhammad@MacBook-Pro---user.local>
2023-08-31 12:13:19 +0500

    feat: admin

--- apps/midjouney/admin.py
+++ apps/midjouney/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Midjouney
+from .models import Midjouney, Struktura, Parametr, ImageParametr
 
 
 @admin.register(Midjouney)
 class MidjouneyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Struktura)
+class StrukturaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Parametr)
+class ParametrAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ImageParametr)
+class ImageParametrAdmin(admin.ModelAdmin):
     pass
