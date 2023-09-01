a6310b6ad147eb9528659e0cf652731caf26ab8e
Nurmuhammad Mashrapov <117863166+nurmuhammad-mashrapov@users.noreply.github.com>
2023-09-01 16:29:53 +0500

    Update models.py

--- apps/neauralnetwork/models.py
+++ apps/neauralnetwork/models.py
@@ -15,32 +15,6 @@ class Tag(TimeStampedModel):
         return self.title
 
 
-class Dostup(TimeStampedModel):
-    PLATNO = 'Платно'
-    BESPLATNO = 'Бесплатно'
-    TRIAL = 'Триал'
-
-    DOSTUP_CHOICES = [
-        (PLATNO, 'Платно'),
-        (BESPLATNO, 'Бесплатно'),
-        (TRIAL, 'Триал'),
-    ]
-
-    title = models.CharField(
-        max_length=255,
-        choices=DOSTUP_CHOICES,
-        default=PLATNO,  # You can set the default value to any of the choices
-        verbose_name="Доступ"
-    )
-
-    class Meta:
-        verbose_name = "Доступ"
-        verbose_name_plural = "Доступ"
-
-    def __str__(self):
-        return self.title
-
-
 
 
 class NeuralNetwork(TimeStampedModel):
@@ -50,7 +24,9 @@ class NeuralNetwork(TimeStampedModel):
     zadacha = models.CharField(max_length=255, verbose_name="Задача")
     tag = models.ManyToManyField(Tag, verbose_name="Сфера применения")
     image = models.ImageField(upload_to="neuralnetwork", verbose_name="Изображение")
-    dostup = models.ManyToManyField(Dostup, verbose_name="Доступ(платно/Бесплатно/Триал)")
+    free = models.BooleanField(default=False, verbose_name="Бесплатно")
+    paid = models.BooleanField(default=False, verbose_name="Платно")
+    trial = models.BooleanField(default=False, verbose_name="Триал")
     icon_18 = models.BooleanField(default=False, verbose_name="Иконка 18+")
     icon_ru = models.BooleanField(default=False, verbose_name="Иконка русский язык")
     slug = models.SlugField(max_length=255, verbose_name="Слаг", unique=True)
